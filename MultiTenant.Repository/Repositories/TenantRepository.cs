9ab13d35031746b2df2757e25bfd184651011e32
Josh Cadruvi <jcadruvi@mobitor.com>
2013-06-05 14:51:44 -0700

    Added the Service and Repository projects.

---
+++ MultiTenant.Repository/Repositories/TenantRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MultiTenant.Repository.Interfaces;
+
+namespace MultiTenant.Repository.Repositories
+{
+    public class TenantRepository : ITenantRepository
+    {
+    }
+}
