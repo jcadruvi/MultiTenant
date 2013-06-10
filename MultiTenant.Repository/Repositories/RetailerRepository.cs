9beadf3f00e3d35e68ed7838658086785662669d
Josh Cadruvi <jcadruvi@mobitor.com>
2013-06-10 11:31:41 -0700

    Starting to add the backend for the Retailer page.

---
+++ MultiTenant.Repository/Repositories/RetailerRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTenant.Repository.Repositories
+{
+    public class RetailerRepository
+    {
+        private static RetailerRepository _instance = new RetailerRepository();
+        static RetailerRepository(){}
+        private RetailerRepository(){}
+        public static RetailerRepository Instance { get { return _instance; } }
+    }
+}
---
+++ MultiTenant.Site/Controllers/RetailerApiController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MultiTenant.Controllers
+{
+    public class RetailerApiController : BaseApiController
+    {
+    }
+}
