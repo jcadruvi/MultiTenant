0808fd0f09333c674415862b31ef59bf2b9a79bc
Josh Cadruvi <jcadruvi@mobitor.com>
2013-06-10 11:34:27 -0700

    Starting to add the back end for the Retailer page.

---
+++ MultiTenant.Model/Retailer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTenant.Model
+{
+    public class Retailer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
