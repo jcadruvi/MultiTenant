a86ba1d4ca00300089b2e55df8dfc7a6c9dd1492
Josh Cadruvi <jcadruvi@mobitor.com>
2013-06-10 14:50:27 -0700

    Added two panels to the Retailer page.

--- MultiTenant.Site/Models/RetailerViewModel.cs
+++ MultiTenant.Site/Models/RetailerViewModel.cs
@@ -7,5 +7,10 @@ namespace MultiTenant.Models
 {
     public class RetailerViewModel: BaseViewModel
     {
+        public string CalendarSales { get; set; }
+        public string FiscalSales { get; set; }
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public string Number { get; set; }
     }
 }
