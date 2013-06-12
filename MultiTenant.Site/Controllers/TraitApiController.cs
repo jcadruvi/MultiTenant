68f073be6bb07d5d831826c06a6dd11b63e0226b
Josh Cadruvi <jcadruvi@mobitor.com>
2013-06-12 16:34:03 -0700

    Added TraitApiController.cs.

---
+++ MultiTenant.Site/Controllers/TraitApiController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MultiTenant.Model;
+using MultiTenant.Service.Interfaces;
+
+namespace MultiTenant.Controllers
+{
+    public class TraitApiController : BaseApiController
+    {
+        private ITraitService _traitService;
+        public TraitApiController(ITraitService traitService)
+        {
+            _traitService = traitService;
+        }
+        public IEnumerable<Trait> GetAddTraits()
+        {
+            return _traitService.GetAddTraits();
+        }
+        public IEnumerable<Trait> GetViewTraits()
+        {
+            return _traitService.GetViewTraits();
+        }
+    }
+}
