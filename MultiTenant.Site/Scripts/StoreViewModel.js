1bde4caff090d63ca2227481568dc44327edad1a
Josh Cadruvi <jcadruvi@mobitor.com>
2013-06-13 10:20:20 -0700

    Store pages collapse all and expand all works.

--- MultiTenant.Site/Scripts/StoreViewModel.js
+++ MultiTenant.Site/Scripts/StoreViewModel.js
@@ -12,35 +12,53 @@
     this.traitViewGridData = null;
 }
 
-StoreViewModel.prototype.onCollapseAllClick = function() {
+StoreViewModel.prototype.doCollapseStore = function () {
+    this.$storeDetail.hide();
+    this.$storeButton.val('+');
+};
 
+StoreViewModel.prototype.doCollapseTrait = function() {
+    this.$traitDetail.hide();
+    this.$traitButton.val('+');
+    this.$traitPanel.removeClass('traitPanelExpanded');
+    this.$traitPanel.addClass('traitPanelCollapsed');
+};
+
+StoreViewModel.prototype.doExpandStore = function() {
+    this.$storeDetail.show();
+    this.$storeButton.val('-');
+};
+
+StoreViewModel.prototype.doExpandTrait = function() {
+    this.$traitDetail.show();
+    this.$traitButton.val('-');
+    this.$traitPanel.addClass('traitPanelExpanded');
+    this.$traitPanel.removeClass('traitPanelCollapsed');
+};
+ 
+StoreViewModel.prototype.onCollapseAllClick = function() {
+    this.doCollapseStore();
+    this.doCollapseTrait();
 };
 
 StoreViewModel.prototype.onExpandAllClick = function() {
-
+    this.doExpandStore();
+    this.doExpandTrait();
 };
 
 StoreViewModel.prototype.onStoreClick = function() {
     if (this.$storeButton.val() === '+') {
-        this.$storeDetail.show();
-        this.$storeButton.val('-');
+        this.doExpandStore();
     } else {
-        this.$storeDetail.hide();
-        this.$storeButton.val('+'); 
+        this.doCollapseStore();
     }
 };
 
 StoreViewModel.prototype.onTraitClick = function() {
     if (this.$traitButton.val() == '+') {
-        this.$traitDetail.show();
-        this.$traitButton.val('-');
-        this.$traitPanel.addClass('traitPanelExpanded');
-        this.$traitPanel.removeClass('traitPanelCollapsed');
+        this.doExpandTrait();
     } else {
-        this.$traitDetail.hide();
-        this.$traitButton.val('+');
-        this.$traitPanel.removeClass('traitPanelExpanded');
-        this.$traitPanel.addClass('traitPanelCollapsed');
+        this.doCollapseTrait();
     }
 };
 
