a0840c55d3ee4a32651add1de42d01d245417d6e
Josh Cadruvi <jcadruvi@mobitor.com>
2013-06-13 10:10:25 -0700

    Trait panel expands and collapses.

--- MultiTenant.Site/Scripts/StoreViewModel.js
+++ MultiTenant.Site/Scripts/StoreViewModel.js
@@ -3,31 +3,46 @@
     this.$storeDetail = $('#storeDetail');
     this.$traitAddButton = $('#traitAddButton');
     this.$traitAddPanel = $('#traitAddPanel');
-    this.$traitAddGridData = null;
+    this.$traitButton = $('#traitButton');
+    this.$traitDetail = $('#traitDetail');
+    this.$traitPanel = $('#traitPanel')
     this.$traitViewPanel = $('#traitViewPanel');
-    this.$traitViewGridData = null;
+    
+    this.traitAddGridData = null;
+    this.traitViewGridData = null;
 }
+
+StoreViewModel.prototype.onCollapseAllClick = function() {
+
+};
+
+StoreViewModel.prototype.onExpandAllClick = function() {
+
+};
 
 StoreViewModel.prototype.onStoreClick = function() {
     if (this.$storeButton.val() === '+') {
         this.$storeDetail.show();
         this.$storeButton.val('-');
-        this.$traitAddPanel.css('height', '400px');
-        this.$traitViewPanel.css('height', '400px');
-        this.$traitAddGridData.refresh();
-        this.$traitViewGridData.refresh();
-        this.$traitAddButton.css('margin-top', '200px');
     } else {
         this.$storeDetail.hide();
-        this.$storeButton.val('+');
-        this.$traitAddPanel.css('height', '500px');
-        this.$traitViewPanel.css('height', '500px');
-        this.$traitAddGridData.refresh();
-        this.$traitViewGridData.refresh();
-        this.$traitAddButton.css('margin-top', '250px');
+        this.$storeButton.val('+'); 
+    }
+};
+
+StoreViewModel.prototype.onTraitClick = function() {
+    if (this.$traitButton.val() == '+') {
+        this.$traitDetail.show();
+        this.$traitButton.val('-');
+        this.$traitPanel.addClass('traitPanelExpanded');
+        this.$traitPanel.removeClass('traitPanelCollapsed');
+    } else {
+        this.$traitDetail.hide();
+        this.$traitButton.val('+');
+        this.$traitPanel.removeClass('traitPanelExpanded');
+        this.$traitPanel.addClass('traitPanelCollapsed');
     }
 };
 
 StoreViewModel.prototype.setObservables = function() {
-
 };
