51b6b339caf0340b276a55f7c31f808da0b5253d
Josh Cadruvi <jcadruvi78@yahoo.com>
2013-06-15 20:07:37 -0700

    Required fields on stores work.

--- MultiTenant.Site/Models/StoreViewModel.cs
+++ MultiTenant.Site/Models/StoreViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,11 @@ namespace MultiTenant.Models
     public class StoreViewModel : BaseViewModel
     {
         public string City { get; set; }
+        [Required]
         public int? Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Number { get; set; }
         public string State { get; set; }
     }
