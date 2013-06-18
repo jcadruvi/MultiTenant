fca810d350f944046be141afe1014eb6bb3ca9d0
Josh Cadruvi <jcadruvi@mobitor.com>
2013-06-18 11:54:20 -0700

    Added the CoreId.

--- MultiTenant.Common/Types/TenantIds.cs
+++ MultiTenant.Common/Types/TenantIds.cs
@@ -8,6 +8,7 @@ namespace MultiTenant.Common.Types
 {
     public static class TenantIds
     {
+        public static readonly int CoreId = 0;
         public static readonly int AppleId = 1;
         public static readonly int MicrosoftId = 2;
         public static readonly int BestBuyId = 3;
