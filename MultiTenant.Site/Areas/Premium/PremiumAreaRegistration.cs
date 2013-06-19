1d594369976e933ada090496cb40989541469861
Josh Cadruvi <jcadruvi@mobitor.com>
2013-06-19 11:27:11 -0700

    Added the Premium area and the RetailerSalesPartial.

---
+++ MultiTenant.Site/Areas/Premium/PremiumAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MultiTenant.Areas.Premium
+{
+    public class PremiumAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Premium";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Premium_default",
+                "Premium/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
