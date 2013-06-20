900f726258d24f0629221cb175fb4eefca4cf8d6
Josh Cadruvi <jcadruvi@mobitor.com>
2013-06-20 11:07:01 -0700

    Added premium retailer content.

---
+++ MultiTenant.Site/Areas/Premium/Controllers/RetailerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MultiTenant.Controllers;
+using MultiTenant.Models;
+using MultiTenant.Service.Interfaces;
+
+namespace MultiTenant.Areas.Premium.Controllers
+{
+    public class RetailerController : BaseController
+    {
+        public RetailerController(ITenantService tenantService)
+            : base(tenantService)
+        {
+        }
+        //
+        // GET: /Apple/Report/
+
+        public ActionResult RetailerInfo()
+        {
+            BaseViewModel model = new BaseViewModel();
+            model.CurrentTenant = CurrentTenant;
+            return View("RetailerInfo", model);
+        }
+
+    }
+}
